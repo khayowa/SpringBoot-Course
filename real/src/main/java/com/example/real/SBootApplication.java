c0e33394e5f22bc6c5eab20319971d847718405d
KHAYO <jozeykhayo@gmail.com>
2022-12-22 10:00:51 +0200

    Initializing spring

---
+++ real/src/main/java/com/example/real/SBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBootApplication.class, args);
+	}
+
+}
