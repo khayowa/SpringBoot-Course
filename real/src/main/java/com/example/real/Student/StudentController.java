ae45670a391484bf8eb5e582b49bbdae899df97a
KHAYO <jozeykhayo@gmail.com>
2022-12-26 12:13:55 +0200

    Configuring registerNewStudent

--- real/src/main/java/com/example/real/Student/StudentController.java
+++ real/src/main/java/com/example/real/Student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.real.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudent(){
         return studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
     }
 }
